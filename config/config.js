009715df5973e1f3f6d9f338dd52d332ffcdde51
Jessigammer1 <jessigammer@gmail.com>
2024-08-27 19:31:45 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 export const dbUrl = "mongodb+srv://botsbazaar:SqeIfowwk7xwcmoE@botsbazaar.sjj4mn8.mongodb.net/?retryWrites=true&w=majority&appName=botsbazaar";
-export const botToken = "6580112130:AAEPpOjNr4jFVOc-0vPjVYU8QlGoKI5jdak";
+export const botToken = "6288837444:AAFy0htn41tqZ_pqpOA6mrzeqCPX3bLjxtI";
 export const admins = [6966849450, 5547959277]
-export const channels = ["zaker_channel_001", "zaker_channel_002"]
+export const channels = ["TC_30SEC_WINGO", "TC_30SEC_WINGO"]
 export const cancelKeyboard = {
     reply_markup: {
         keyboard: [
